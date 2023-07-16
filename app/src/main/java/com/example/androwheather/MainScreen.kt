a7e183095ad872d4ab6ddcdd4c47ed31f494decf
AngelinShp <angelinashpspb@gmail.com>
2023-07-16 23:48:35 +0300

    Верхняя часть главного экрана

---
+++ app/src/main/java/com/example/androwheather/MainScreen.kt
@@ -0,0 +1,124 @@
+package com.example.androwheather
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.androwheather.R
+import com.example.androwheather.ui.theme.Pink40
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    Image(
+        painter = painterResource(
+            id = R.drawable.background
+        ),
+        contentDescription = "im1",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Card(modifier = Modifier.fillMaxWidth())
+        {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "20 Jun 2022 13:00",
+                        modifier = Modifier.padding(
+                            top = 8.dp,
+                            start = 8.dp
+                        ),
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        contentDescription = "im2",
+                        modifier = Modifier
+                            .padding(
+                                top = 8.dp,
+                                end = 8.dp
+                            )
+                            .size(35.dp)
+                    )
+                }
+                Text(
+                    text = "Madrid",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23ºC",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_search_24),
+                            contentDescription = "im3",
+                            tint = Color.White
+                        )
+                    }
+
+                    Text(
+                        text = "23ºC/12ºC",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_cloud_sync_24),
+                            contentDescription = "im4",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
