ba8a417aeeb9cf6c554f73e3f420248367eb0699
AngelinShp <angelinashpspb@gmail.com>
2023-07-19 13:26:44 +0300

    Последнее

--- app/src/main/java/com/example/androweather/MainActivity.kt
+++ app/src/main/java/com/example/androweather/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.androweather
 
 import android.Manifest
+import android.provider.Settings
 import android.app.Instrumentation.ActivityResult
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -26,6 +29,8 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+
 import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
@@ -33,6 +38,10 @@ import com.android.volley.toolbox.Volley
 import com.example.androweather.dataclasses.DataModel
 
 import com.example.androweather.ui.theme.AppForWheatherTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 import org.json.JSONObject
 
 const val API_KEY = "d541c71a030e4f9e90a221843231707"
@@ -40,6 +49,7 @@ const val API_KEY = "d541c71a030e4f9e90a221843231707"
 
 class MainActivity : ComponentActivity() {
     private lateinit var pLauncher: ActivityResultLauncher<String>
+    private lateinit var fLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -91,30 +101,61 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        registerPermissionListener()
+        permissionListener()
         checkPermission()
     }
-    private fun checkPermission(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            when {
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED -> {
-    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show()
-                }
-
-                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
-                -> {
-                    Toast.makeText(this, "Permission need!", Toast.LENGTH_LONG).show()
-
-                }
-
-                else -> {
-                    pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                }
-            }
-        }
-
+    override fun onResume() {
+        super.onResume()
+        checkLocation()
     }
-    private fun registerPermissionListener(){
+    private fun checkLocation(){
+        if(isLocationEnabled()){
+            getLocation()
+        } else {
+            DialogManager.locationSettingsDialog(this, object : DialogManager.Listener{
+                override fun onClick() {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+            })
+        }
+    }
+    private fun isLocationEnabled(): Boolean{
+        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun getLocation(){
+
+        if(!isLocationEnabled()){return}
+        val ct = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fLocationClient
+            .getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, ct.token)
+            .addOnCompleteListener{
+                getData("${it.result.latitude},${it.result.longitude}", this, daysList, currentDay)
+            }
+    }
+    fun isPermissionGranted(p: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this, p) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+    private fun permissionListener(){
+        fLocationClient = LocationServices.getFusedLocationProviderClient(this)
         pLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()) {
         if(it){
@@ -152,14 +193,14 @@ private fun getData(    city: String, context: Context,
 }
 
 
-private fun getWeatherByDays(response: String): List<DataModel> {
+private fun getWeatherByDays(response: String): List<DataModel>{
     if (response.isEmpty()) return listOf()
     val list = ArrayList<DataModel>()
     val mainObject = JSONObject(response)
     val city = mainObject.getJSONObject("location").getString("name")
     val days = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
 
-    for (i in 0 until days.length()) {
+    for (i in 0 until days.length()){
         val item = days[i] as JSONObject
         list.add(
             DataModel(
@@ -183,3 +224,11 @@ private fun getWeatherByDays(response: String): List<DataModel> {
     )
     return list
 }
+
+
+
+
+
+
+
+
